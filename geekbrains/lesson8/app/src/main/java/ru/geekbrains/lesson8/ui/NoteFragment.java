82952fd9a29801d0491c793e4c282e8fb1e3ab01
Zurbaevi Nika <zurbaevi.nika@gmail.com>
2021-03-20 00:11:24 +0400

    Homework done for lesson 8

---
+++ geekbrains/lesson8/app/src/main/java/ru/geekbrains/lesson8/ui/NoteFragment.java
@@ -0,0 +1,59 @@
+package ru.geekbrains.lesson8.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+import ru.geekbrains.lesson8.Note;
+import ru.geekbrains.lesson8.R;
+
+/**
+ * @author Zurbaevi Nika
+ */
+public class NoteFragment extends Fragment {
+
+    public static final String CURRENT_NOTE = "currentNote";
+    private Note note;
+
+    public static NoteFragment newInstance(Note note) {
+        NoteFragment fragment = new NoteFragment();
+        Bundle args = new Bundle();
+        args.putParcelable(CURRENT_NOTE, note);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            note = getArguments().getParcelable(CURRENT_NOTE);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_note, container, false);
+        TextInputEditText titleText = view.findViewById(R.id.note_title);
+        TextInputEditText contentText = view.findViewById(R.id.note_content);
+        TextView dateOfCreationText = view.findViewById(R.id.note_date_of_creation);
+        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss dd-MM-yyyy",
+                Locale.getDefault());
+        dateOfCreationText.setText(String.format("%s: %s", "Дата создания",
+                formatter.format(note.getCreationDate().getTime())));
+        titleText.setText(note.getTitle());
+        contentText.setText(note.getContent());
+        view.setBackgroundColor(note.getColor());
+        setHasOptionsMenu(true);
+        return view;
+    }
+}
