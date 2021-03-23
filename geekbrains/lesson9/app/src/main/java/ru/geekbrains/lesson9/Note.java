19a2f0bf3eda25afd4656ba6897df56340bdbff0
Zurbaevi Nika <zurbaevi.nika@gmail.com>
2021-03-23 21:34:06 +0400

    Initial project

---
+++ geekbrains/lesson9/app/src/main/java/ru/geekbrains/lesson9/Note.java
@@ -0,0 +1,71 @@
+package ru.geekbrains.lesson9;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.util.Calendar;
+
+/**
+ * @author Zurbaevi Nika
+ */
+public class Note implements Parcelable {
+    public static final Creator<Note> CREATOR = new Creator<Note>() {
+        @Override
+        public Note createFromParcel(Parcel in) {
+            return new Note(in);
+        }
+
+        @Override
+        public Note[] newArray(int size) {
+            return new Note[size];
+        }
+    };
+    private String title;
+    private String content;
+    private Calendar creationDate;
+    private int color;
+
+    public Note(String title, String content, Calendar creationDate, int color) {
+        this.title = title;
+        this.content = content;
+        this.creationDate = creationDate;
+        this.color = color;
+    }
+
+    protected Note(Parcel in) {
+        title = in.readString();
+        content = in.readString();
+        creationDate = (Calendar) in.readSerializable();
+        color = in.readInt();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(title);
+        dest.writeString(content);
+        dest.writeSerializable(creationDate);
+        dest.writeInt(color);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Calendar getCreationDate() {
+        return creationDate;
+    }
+
+    public int getColor() {
+        return color;
+    }
+}
+
---
+++ geekbrains/lesson9/app/src/main/java/ru/geekbrains/lesson9/ui/NotesAdapter.java
@@ -0,0 +1,90 @@
+package ru.geekbrains.lesson9.ui;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+import ru.geekbrains.lesson9.Note;
+import ru.geekbrains.lesson9.R;
+
+/**
+ * @author Zurbaevi Nika
+ */
+public class NotesAdapter extends RecyclerView.Adapter<ru.geekbrains.lesson9.ui.NotesAdapter.ViewHolder> {
+    private Note[] notes;
+    private MyClickListener myClickListener;
+
+    public NotesAdapter(Note[] notes) {
+        this.notes = notes;
+    }
+
+    public void setOnItemClickListener(MyClickListener itemClickListener) {
+        myClickListener = itemClickListener;
+    }
+
+    @NonNull
+    @Override
+    public ru.geekbrains.lesson9.ui.NotesAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ru.geekbrains.lesson9.ui.NotesAdapter.ViewHolder holder, int position) {
+        holder.getItemLayout().setBackgroundColor(notes[position].getColor());
+        holder.getTitleTextView().setText(notes[position].getTitle());
+        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss dd-MM-yyyy",
+                Locale.getDefault());
+        holder.getDateTextView().setText(formatter.format(notes[position].getCreationDate().getTime()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return notes.length;
+    }
+
+    public interface MyClickListener {
+        void onItemClick(int position, Note note);
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private CardView cardView;
+        private LinearLayout itemLayout;
+        private TextView titleTextView;
+        private TextView dateTextView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            cardView = (CardView) itemView;
+            itemLayout = itemView.findViewById(R.id.element_of_recycler_view);
+            titleTextView = itemView.findViewById(R.id.first_tv_of_item);
+            dateTextView = itemView.findViewById(R.id.second_tv_of_item);
+            itemLayout.setOnClickListener(v -> {
+                int position = getAdapterPosition();
+                myClickListener.onItemClick(position, notes[position]);
+            });
+        }
+
+        public LinearLayout getItemLayout() {
+            return itemLayout;
+        }
+
+        public TextView getTitleTextView() {
+            return titleTextView;
+        }
+
+        public TextView getDateTextView() {
+            return dateTextView;
+        }
+    }
+}
